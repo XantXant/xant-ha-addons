25ed49cf87c74f6346bb190d14951a03ca576d0b
XantXant <alexander.holzleitner@gmail.com>
2024-07-01 20:31:16 +0200

    commit dev v1.0.20 mqtt test 4

--- batterycharge-dev/rootfs/run.sh
+++ batterycharge-dev/rootfs/run.sh
@@ -5,15 +5,14 @@
 if ! bashio::services.available "mqtt"; then
     # bashio::exit.nok "No internal MQTT Broker found. Please install Mosquitto broker."
     MQTT_HOST="nooo"
+    echo "$MQTT_HOST asdfS" > /mqtt.txt
 else
-#     MQTT_HOST=$(bashio::services mqtt "host")
-#     MQTT_PORT=$(bashio::services mqtt "port")
-#     MQTT_USER=$(bashio::services mqtt "username")
-#     MQTT_PASS=$(bashio::services mqtt "password")
-#     bashio::log.info "Configured'$MQTT_HOST' mqtt broker."
-#     echo "$MQTT_HOST $MQTT_PORT $MQTT_USER $MQTT_PASS" > /mqtt.txt
-    MQTT_HOST="juhu"
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASS=$(bashio::services mqtt "password")
+    bashio::log.info "Configured '$MQTT_HOST' mqtt broker."
+    echo "$MQTT_HOST $MQTT_PORT $MQTT_USER $MQTT_PASS" > /mqtt.txt
 fi
-echo "$MQTT_HOST asdfS" > /mqtt.txt
 
 python /app/main.py notest
