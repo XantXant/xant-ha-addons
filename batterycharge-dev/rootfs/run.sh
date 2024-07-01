2b7f147d26c28b9010b67e3b2e42709614f66a5a
XantXant <alexander.holzleitner@gmail.com>
2024-07-01 19:05:09 +0200

    commit dev v1.0.19 mqtt test 3

---
+++ batterycharge-dev/rootfs/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bashio
+
+bashio::log.info "Get MQTT Data n..."
+
+if ! bashio::services.available "mqtt"; then
+   bashio::exit.nok "No internal MQTT Broker found. Please install Mosquitto broker."
+else
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_PORT=$(bashio::services mqtt "port")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASS=$(bashio::services mqtt "password")
+    bashio::log.info "Configured'$MQTT_HOST' mqtt broker."
+fi
