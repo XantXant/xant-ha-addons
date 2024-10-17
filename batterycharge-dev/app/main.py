6956abb668abfe2df606c7dc57829106ac535e14
XantXant <alexander.holzleitner@gmail.com>
2024-10-17 20:12:47 +0200

    commit dev v1.0.24 mqtt test 5

--- batterycharge-dev/app/main.py
+++ batterycharge-dev/app/main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     print(sys.argv[3])
     print(sys.argv[4])
     mqtt_host = sys.argv[1]
-    mqtt_port = sys.argv[2]
+    mqtt_port = int(sys.argv[2])
     mqtt_user = sys.argv[3]
     mqtt_pass = sys.argv[4]
 
